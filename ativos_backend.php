53acc420035234f412d9f07b7c44628947926861
Gabriel Schmitz <60523353+ssWyvern@users.noreply.github.com>
2023-09-28 22:18:36 -0300

    Update ativos_backend.php

--- ativos_backend.php
+++ ativos_backend.php
@@ -2,5 +2,47 @@
 
 include 'ativos_sql.php';
 
+require_once("lib/xajax/xajax.inc.php");
+
+$xajax = new xajax();
+$xajax->setCharEncoding('UTF-8');
+$xajax->processRequest();
+
+function salvar_carteira($dados) {
+
+    $editar = $dados['editar'];
+    $idCliente = $dados['idcliente'];
+    $descricaoCarteira = $dados['descricaoCarteira'];
+    if ($descricaoCarteira != '') {
+        if ($editar = 1) {
+            alteraCarteira($descricaoCarteira, $idCliente);
+        } else {
+            cadastroCarteira($descricaoCarteira, $idCliente);
+        }
+        return 1;
+    } else return 0;
+}
+
+function vizulizar_carteira($dados){
+    $idCarteira = $dados['idCarteira'];
+    $result = listaCarteiras($idCarteira);
+
+    $carteira = mysqli_fetch_array($result);
+
+    return $carteira["DESCRICAO"];
+}
+
+function listar_carteiras($dados){
+    $result = listaCarteiras(-1);
+    $carteiras = array();
+    if ($result > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $idCarteira = $row["ID"];
+            $descricaoCarteira = $row["DESCRICAO"];
+            $carteiras[] = array($idCarteira, $descricaoCarteira);
+        }
+    }
+    return $carteiras;
+}
 
 ?>
