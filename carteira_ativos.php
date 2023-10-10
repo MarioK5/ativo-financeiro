8dd01ee085ed42ef09baed6d77e53d71e9aeb325
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-10 13:07:06 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -112,7 +112,7 @@ function busca_carteiras($idCliente)   {
 	$tela .= '<table border="0" width=100%>
 			    <div class="row">
 				<div class="col-xs-6 col-md-2">
-				    <input type="button" value="Criar Nova Carteira"  class="btn btn-success btn-sm" onclick="xajax_cadastrar_carteira(xajax.getFormValues('form_cadastro','.$idCliente.')); return false;">
+				    <input type="button" value="Criar Nova Carteira"  class="btn btn-success btn-sm" onclick="xajax_cadastrar_carteira('.$idCliente.'); return false;">
 				    <input type="text" class="form-control" name="desc_carteira" id="desc_carteira" value=""/>
 				</div>
 			    </div>
@@ -144,7 +144,7 @@ function busca_carteiras($idCliente)   {
 	return $resp;
 }
 
-function cadastrar_carteira($dados,$idCliente)   {
+function cadastrar_carteira($idCliente)   {
 
 	$resp = new xajaxResponse();
 
