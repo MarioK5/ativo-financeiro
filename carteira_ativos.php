5f73d5846d0e305f12a6c389c9ee3c75e0c61271
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-09 21:21:50 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -127,7 +127,11 @@ function busca_carteiras($idCliente)   {
 					<td> # </td>
                 </tr> ';	
         	}
-			
+		<tr style="color:white; background-color:#8ecae6;">
+                    <td rowspan="3">
+      			<input type="button" value="Nova Carteira"  class="btn btn-secondary btn-sm" onclick="xajax_cadastrar_carteira('.$idCliente.'); return false;">
+      		    </td>
+                </tr>	
 		$tela .= '</table>';	
 	}
 
