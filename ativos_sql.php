14624cda8b422cc82ba6e7c4053e5f57fe9ea4aa
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-11 08:05:27 -0300

    Update ativos_sql.php

--- ativos_sql.php
+++ ativos_sql.php
@@ -271,6 +271,9 @@ function alteraCarteira($descricao, $idCliente, $idCarteira){
     		WHERE ID_CLIENTE = '{$idCliente}'
       		  AND ID = '{$idCarteira}'";
 
+	$arq = fopen("log.txt","w") or die("Problemas para criar o arquivo");
+        fputs($arq,$sql);
+        fclose($arq);
 	$result = mysqli_query($conn,$sql);
 		  mysqli_commit($conn);
 
