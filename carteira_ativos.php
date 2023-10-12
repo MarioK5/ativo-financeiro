6da3208604e2c772f6835b3b24124ee76992406b
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-12 18:11:55 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -259,7 +259,7 @@ function busca_ativos($idCliente)   {
 
 	if (mysqli_num_rows($result2) > 0) {
 		while ($row1 = mysqli_fetch_array($result2)) {
-			$soma_valores .= ($qtde_ativos * $valor_atual_ativo);
+			$soma_valores .= ($row1["QTDE_ATIVOS"] * $row1["VALOR_ATUAL_ATIVO"]);
 		}
 			
 		if (mysqli_num_rows($result2) > 0) {
