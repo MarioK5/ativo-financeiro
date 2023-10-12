61191a464329ea4df95935254e08cf43cc9c778e
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-12 08:07:48 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -227,7 +227,7 @@ function busca_ativos($idCliente)   {
             		$descricao  = $row["DESCRICAO"];
             		$idCliente  = $row["ID_CLIENTE"];
 					
-		$tela .= '<table border="0" width=100%>
+		$tela .= '<table border="0" width=100% class="table">
 			    <div class="row">
     				<div class="col-xs-6 col-md-4">
 					<tr style="background-color:#C0C0C0;">
