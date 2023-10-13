e0bae07fa2de23f65bfbee2a51f9a8dfb9ec320f
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-13 16:24:19 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -246,13 +246,14 @@ function busca_ativos($idCliente)   {
             		$idCarteira = $row["ID"];
             		$descricao  = $row["DESCRICAO"];
             		$idCliente  = $row["ID_CLIENTE"];
+			$teste = "teste";
 					
 		$tela .= '<div class="row">
     				<div class="col-xs-6 col-md-4">
 					<tr style="color:white; background-color:#2F4F4F;">
 				     	     <th colspan="7">'.$descricao.'</th>
 	       				     <th colspan="1" style="text-align: right;">
-							<button type="button" class="btn btn-default btn-xs" onclick="xajax_editar_ativo_carteira('.$idCarteira.',1);">
+							<button type="button" class="btn btn-default btn-xs" onclick="xajax_editar_ativo_carteira('.$idCarteira.','.$teste.');">
 							<span class="glyphicon glyphicon-edit"> Editar</span>
 							</button>
 					     </th>
