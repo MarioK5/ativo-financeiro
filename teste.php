0a9860475422a0e5141803044925244521f7e556
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-15 20:32:24 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -6,16 +6,9 @@
 		jQuery AutoComplete selection 
 	</title> 
 
-	<link rel="stylesheet" href= 
-"http://code.jquery.com/ui/1.11.3/themes/ui-lightness/jquery-ui.css"/> 
-	
-	<script src= 
-		"http://code.jquery.com/jquery-2.1.3.js"> 
-	</script> 
-	
-	<script src= 
-		"http://code.jquery.com/ui/1.11.2/jquery-ui.js"> 
-	</script> 
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.3/themes/ui-lightness/jquery-ui.css"/> 
+	<script src="http://code.jquery.com/jquery-2.1.3.js"></script> 
+	<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script> 
 	
 	<script> 
 		$(document).ready(function() { 
