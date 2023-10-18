01ba4f4b1b4d5c61a4a92406e85f9f1d8d5a2b61
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-18 20:58:59 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -30,7 +30,7 @@ function busca_dados($dados)   {
 	$resp = new xajaxResponse();
 	
 
-	$resp->alert('O e-mail é : '$dados['email']); return $resp;
+	$resp->alert('O e-mail é : '.$dados['email']); return $resp;
 
 	$tela  = '';
 
