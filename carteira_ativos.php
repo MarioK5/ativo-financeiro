73496176e69e76858813d9eac04612d7f8939513
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-18 19:57:42 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -684,6 +684,8 @@ function recuperar_senha()   {
 <!DOCTYPE html> 
 
 <html> 
+<%@ page language="java" contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
     <head>
         <title>Carteira de Ativos IFRS</title>
 
