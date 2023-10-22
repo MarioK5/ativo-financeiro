971ff0151ea5856c6984c6a38c8f064c8d8434c4
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-22 00:01:13 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -22,6 +22,7 @@ $xajax->registerFunction("tipo_ativo");
 $xajax->registerFunction("ativo_select");
 $xajax->registerFunction("cadastrar_investimento");
 $xajax->registerFunction("destinar_investimento");
+$xajax->registerFunction("historico_carteira");
 $xajax->registerFunction("cadastrar_cliente");
 $xajax->registerFunction("recuperar_senha");
 $xajax->processRequest();
@@ -567,7 +568,7 @@ function busca_investimentos($idCliente)   {
 				     	     <th colspan="3">'.$descricao[$ind].'</th>
 	       				     <th colspan="4">R$ '.number_format($valorCarteira[$ind],2,",",".").'</th>
 	  				     <th colspan="1" style="text-align: right;">
-	    					 <button type="button" class="btn btn-default btn-xs" data-toggle="modal" data-target="#historicoModal" onclick="historico_carteira('.$idCarteira[$ind].');">
+	    					 <button type="button" class="btn btn-default btn-xs" onclick="xajax_historico_carteira('.$idCarteira[$ind].');">
 							<span class="glyphicon glyphicon-time"> Histórico</span>
 							</button>
 				   		 <input type="button" value="Adicionar Investimento" class="btn btn-success btn-xs" onclick="xajax_cadastrar_investimento('.$idCliente[$ind].','.$idCarteira[$ind].')">
@@ -949,34 +950,19 @@ function destinar_investimento($valorInvest, $idCarteira, $idCliente)   {
 }
 
 function historico_carteira()   {
+	
+	$resp = new xajaxResponse("UTF-8");
 
-	$tela '<div class="modal fade" id="historicoModal" tabindex="-1" role="dialog" aria-labelledby="historicoModalLabel" aria-hidden="true">
-		  <div class="modal-dialog" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <h5 class="modal-title" id="historicoModalLabel">Histórico de Investimento</h5>
-			
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		          <span aria-hidden="true">&times;</span>
-		        </button>
-		      </div>
-		      <div class="modal-body">
-		        ...
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-		
-		 ';
+
+  
+	$resp->alert('Histórico de Investimento!'); return $resp;
 
 	
 
 
+	$resp->assign("tela_saida","innerHTML",$tela);
   
-	return $tela;
+	return $resp;
 }
 
 function cadastrar_cliente()   {
