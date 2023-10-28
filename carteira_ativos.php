40407ad2fb0c0d3fba2ea5880305d939940fb6ac
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-10-28 19:08:50 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -1233,7 +1233,7 @@ function cadastrar_cliente()   {
     				  </div>
 			    </div>
        			    <div class="row">
-	  			  <div class="col-xs-3 col-md-3">
+	  			  <div class="col-xs-2 col-md-2">
     				    <div class="form-group">
                                         <div id="sandbox-container">
                                             <div class="input-group">
@@ -1242,7 +1242,7 @@ function cadastrar_cliente()   {
                                         </div>
                                      </div>
     				  </div>
-				  <div class="col-xs-3 col-md-3">
+				  <div class="col-xs-4 col-md-4">
     				    <div class="form-group">
                                         <div id="sandbox-container">
                                             <div class="input-group">
