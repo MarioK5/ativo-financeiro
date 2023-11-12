93f17202b6594b44c69d8c08501bbb797dee02c5
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-11-11 23:19:41 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -1104,8 +1104,8 @@ function destinar_investimento($valorInvest, $idCarteira, $idCliente)   {
 				<th>Valor<br>Investido</th>
 				<th>Valor Atual<br>Ativo</th>
 				<th>% Atual</th>
+    				<th>==></th>
     				<th>Novo %</th>
-				<th>==></th>
 				<th>Qtde Ativos<br>Sugerido</th>
     				<th>Valor sugerido</th>
 			  </tr> ';	
@@ -1162,10 +1162,10 @@ function destinar_investimento($valorInvest, $idCarteira, $idCliente)   {
 								<td>'.number_format($valor_atual_investido,2,",",".").'</td>
 								<td>'.number_format($valor_atual_ativo[$ind],2,",",".").'</td>
 								<td>'.number_format($perc_atual,2,",",".").'</td>
+								<td>==></td>
 								<td>
 									<input type="text" class="form-control" name="novoPerc[]'.$ind.'" id="novoPerc[]'.$ind.'" value="'.number_format($novo_perc,2,",",".").'" readonly="readonly" style="width: 70px;" />
 							 	</td>
-								<td>==></td>
 								<td>
 									<input type="text" class="form-control" name="n_newAtivos[]'.$ind.'" id="n_newAtivos[]'.$ind.'" value="'.number_format($ativosSugeridos,0,",",".").'" readonly="readonly" style="width: 45px;" />
 								</td>
