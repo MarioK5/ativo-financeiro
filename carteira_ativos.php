23ea36a6d945dcdf742e05efbf5083d501240d69
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-11-17 08:05:50 -0300

    Update carteira_ativos.php

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -1128,7 +1128,7 @@ function destinar_investimento($valorInvest, $idCarteira, $idCliente)   {
 					$valor_atual_investido = ($lista[$z]["QTDE_ATIVOS"] * $lista[$z]["VALOR_ATUAL_ATIVO"]);
 	
 					if($lista[$z]["SUGERIDO"] > 0) {
-						$valorSugerido = ($lista[$z["SUGERIDO"] + (($lista[$z]["SUGERIDO"] / $somaPositivo ) * ($somaNegativo )));
+					//	$valorSugerido = ($lista[$z["SUGERIDO"] + (($lista[$z]["SUGERIDO"] / $somaPositivo ) * ($somaNegativo )));
 						
 					}else{
 						$valorSugerido = 0;
