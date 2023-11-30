433eda20b04742296976c5b86de05736e10c515d
MarioK5 <143139421+MarioK5@users.noreply.github.com>
2023-11-30 20:16:13 -0300

    Update carteira_ativos.php
    ajuste

--- carteira_ativos.php
+++ carteira_ativos.php
@@ -812,7 +812,7 @@ function confirmar_inativar_carteira($idCliente, $idCarteira)   {
 						<div id="sandbox-container">
 							<div class="input-group">
 								<div class="form-group" style="font-size: 18px;">
-									Tem certeza que deseja excluir a carteira '.descrCarteira.'?
+									Tem certeza que deseja excluir a carteira '.$descrCarteira.'?
 								</div>
 							</div>
 						</div>
